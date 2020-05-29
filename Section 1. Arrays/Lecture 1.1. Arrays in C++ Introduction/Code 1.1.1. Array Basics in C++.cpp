b839270b50730ad8f60d7b528879e0f0a0e4c44c
Aditya <adityakumar28009@gmail.com>
2020-05-29 07:35:24 +0530

    Complete Lecture 1.1

---
+++ Section 1. Arrays/Lecture 1.1. Arrays in C++ Introduction/Code 1.1.1. Array Basics in C++.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void get(int a[], int n)
+{
+    /* To input 'n' values into an array */
+
+    cout << "Enter the elements of the array separated by spaces or newline: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+void display(int a[], int n)
+{
+    /* To display first 'n' values stored in an array */
+
+    cout << "Elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+int main()
+{
+    /* Code Execution Starts here */
+
+    // An initialisation using this format initialises the number of elements as present in {} and rest by 0
+    int arr[100] = {0};
+    int n;
+
+    cout << "Enter number of elements in the array: ";
+    cin >> n;
+
+    get(arr, n);
+    display(arr, n);
+
+    return 0;
+}
